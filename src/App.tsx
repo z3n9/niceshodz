b7b0b970196fc48f066cb3bd62d932b04133f753
Jackey Zeng <78958409+z3n9@users.noreply.github.com>
2025-03-25 15:36:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
+          <Route path="/" element={<Navigate to="/niceshodz" replace />} />
           <Route path="/niceshodz" element={<Home />} />
           <Route path="/who-we-are" element={<WhoWeAre />} />
           <Route path="/what-we-do" element={<WhatWeDo />} />
